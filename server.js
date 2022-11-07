9f73fb85f5843f07b3cfe4ebea136cc9cb87eebf
YunkiJung <100543183+YunkiJung@users.noreply.github.com>
2022-11-07 11:54:38 -0500

    setup frontend folder structure

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
-import express from 'express'
+import express from 'express';
+
+import path,{dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+import cookieParser from 'cookie-parser';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+
+
 const app = express();
 const Port = 3000;
 
+app.set('views', path.join(__dirname, './app/views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, './client')));
+app.use(express.static(path.join(__dirname, './node_modules')));
+
+
 app.get('/', (req, res) => {
-    res.send('Localhost:3000 project started');
+    //res.send('Localhost:3000 project started');
+    res.render('index', { page: 'home' });
 })
 
 app.listen(Port, () => {

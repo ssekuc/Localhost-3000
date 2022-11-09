07df60dc86c0f52928d9fffb7dc1477005f0422a
Timothy Issac Thomas <38766009+Timothy965@users.noreply.github.com>
2022-11-08 19:30:38 -0500

    db url

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+module.exports = {
+    //local MongoDB deployment ->
+    /* URI:
+      "mongodb + srv://localhost3000:TygerTyger@cluster0.ne57vca.mongodb.net/?retryWrites=true&w=majority", */
+    URI:
+        "mongodb + srv://localhost3000:TygerTyger@cluster0.ne57vca.mongodb.net/?retryWrites=true&w=majority"
+
+};
+
+
+

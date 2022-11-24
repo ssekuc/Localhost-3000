e1c5d4a1ef7371cee2ab3a00646e1a6e6c13e797
YunkiJung <100543183+YunkiJung@users.noreply.github.com>
2022-11-24 01:36:40 -0500

    Revert "edited hard coding part in game controller add method"
    This reverts commit a8b6e8e64291006158a32b10b62b9a165f2c48ae.

--- app/game/game.controller.js
+++ app/game/game.controller.js
@@ -16,43 +16,31 @@ export function ProcessAddTournament(req, res, next){
         return res.redirect('/login')
     }
 
-    console.log(req.body.numOfPlayers);
-    console.log(req.body.title);
-
-    function convert(raw){
-        let result = Number(raw);
-        return result;
-    }
-
-    let count = convert(req.body.numOfPlayers);
-
-    console.log(count);
-
     let newGame = new Game({
-        title : req.body.title,
-        creatorId : req.user.username,
-        teams : count,
+        title : "second game",
+        creatorId : "yun",
+        teams : 8,
         game : {
             firstRound : {
                 isDone : "N",
                 game1 : {
-                    team1 : req.body.team1Name,
-                    team2 : req.body.team2Name,
+                    team1 : "yun",
+                    team2 : "alex",
                     winner : null
                 },
                 game2 : {
-                    team3 : req.body.team3Name,
-                    team4 : req.body.team4Name,
+                    team3 : "asdfas",
+                    team4 : "asdfasdfasd",
                     winner : null
                 },
                 game3 : {
-                    team5 : req.body.team5Name,
-                    team6 : req.body.team6Name,
+                    team5 : "teasdfm5",
+                    team6 : "teaasdasdfasm6",
                     winner : null
                 },
                 game4 : {
-                    team7 : req.body.team7Name,
-                    team8 : req.body.team8Name,
+                    team7 : "sssss",
+                    team8 : "teasdf",
                     winner : null
                 }
             },

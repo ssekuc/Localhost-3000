6ade709d21bd6a13cc25bb6013e29b234e07988c
Alex M <amaynar4@my.centennialcollege.ca>
2022-12-04 16:50:56 -0600

    Added Admin page to display all users

--- app/admin/admin.controller.js
+++ app/admin/admin.controller.js
@@ -1,8 +1,16 @@
+import User from "../user/user.model.js";
+
 //Show admin home page
 export function DisplayAdminHomePage(req, res, next){
     if(!req.user){
         return res.redirect('/user/login')
     }
 
-    return res.render('index', { page: 'adminHome', user: req.user });
+    User.find((err, users) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        return res.render('index', { page: 'adminHome', user: req.user, users });
+    })
 }
